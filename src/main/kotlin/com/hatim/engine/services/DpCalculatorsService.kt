e6d8188bad90da4a73ceb78843b7215f5ba7a35c
Hatim <me@me.com>
2019-02-27 13:17:55 +0000

    incremental calculators start

--- src/main/kotlin/com/hatim/engine/services/DpCalculatorsService.kt
+++ src/main/kotlin/com/hatim/engine/services/DpCalculatorsService.kt
@@ -3,33 +3,36 @@ package com.hatim.engine.services
 import com.netflix.discovery.EurekaClient
 import com.netflix.discovery.EurekaEvent
 import com.netflix.discovery.EurekaEventListener
+import com.netflix.discovery.shared.Application
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
-import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicBoolean
 
 @Component
 class DpCalculatorsService(@Autowired val discoveryClient: EurekaClient,
-                           private val calculatorsNbr: Int = 5,
-                           private val waitBeforeCreatingCalculator:Int = 15):
+                           private val calculatorsNbr: Int = 5):
         EurekaEventListener, ApplicationRunner {
     companion object {
         private val logger = LoggerFactory.getLogger(DpCalculatorsService::class.java)
     }
 
-    private val attempts = AtomicInteger()
+    private var calculatorsCurrentNbr = 0
+    private val initialCalculator = AtomicBoolean(true)
 
     override fun onEvent(event: EurekaEvent?) {
-        val engineInstances = discoveryClient.getApplication("dpCalculator")
-        if ((engineInstances == null || engineInstances.size() < calculatorsNbr)
-                && attempts.getAndIncrement() >= waitBeforeCreatingCalculator){
-            val currentNbrOfCalculators = engineInstances?.size() ?: 0
-            val calculatorsToStart = calculatorsNbr - currentNbrOfCalculators
-            logger.info("Found only $currentNbrOfCalculators calculators, Starting $calculatorsToStart")
-            repeat(calculatorsToStart) { launchDpCalculator() }
-            attempts.set(0)
+        val calculatorInstances: Application? = discoveryClient.getApplication("dpCalculator")
+        val updatedNbrOfCalculators = calculatorInstances?.size() ?: 0
+
+        if (calculatorsCurrentNbr != updatedNbrOfCalculators || initialCalculator.get()) {
+            calculatorsCurrentNbr = updatedNbrOfCalculators
+            if (calculatorsCurrentNbr < calculatorsNbr) {
+                logger.info("Found only $updatedNbrOfCalculators calculators. Starting a new one")
+                launchDpCalculator()
+                initialCalculator.compareAndSet(true, false)
+            }
         }
     }
 
