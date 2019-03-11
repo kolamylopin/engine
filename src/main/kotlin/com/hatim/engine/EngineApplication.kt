70e2f3b6dcfcb826e26575fa06985cdded77492c
Hatim <me@me.com>
2019-03-11 08:10:27 +0000

    Migrated away from a web application

--- src/main/kotlin/com/hatim/engine/EngineApplication.kt
+++ src/main/kotlin/com/hatim/engine/EngineApplication.kt
@@ -1,12 +1,22 @@
 package com.hatim.engine
 
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
 @EnableDiscoveryClient
-class EngineApplication
+class EngineApplication : CommandLineRunner {
+	companion object {
+		private val logger = LoggerFactory.getLogger(EngineApplication::class.java)
+	}
+
+	override fun run(vararg args: String?) {
+		logger.info("Application started")
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<EngineApplication>(*args)
